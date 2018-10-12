00288f4176249ff7d6c28249992ed708d8e9e27f
Jiamin Lim <jiaminlim93@gmail.com>
2018-10-12 16:27:11 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,6 +1,10 @@
 // declare your functions here...
 $(document).ready(function() {
   function paragraphSelector(){
-    $('p')
-  }// code to be executed goes here
+    return $('p')
+  }
+  function lastImageSelector(){
+    return $('img:last')
+  }
+  // code to be executed goes here
 });
